82b77b1cecd188e2119dccfc5bbd328e28eefe22
Shusuke1729 <politicalcom770@gmail.com>
2024-12-02 17:17:27 +0900

    Create fukui_tokutyou.php

---
+++ fukui_tokutyou.php
@@ -0,0 +1,268 @@
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>福井観光</title>
+  <style>
+
+    body {
+        font-family: 'Helvetica Neue', Arial, sans-serif;
+        background-color: #f5f5f5;
+        color: #333;
+        margin: 0;
+        padding: 0;
+      }
+
+    #title {
+            margin-top: 0;
+            background: linear-gradient(135deg, #0066cc, #ff69b4);
+            position: relative;
+            height: 150px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        }
+
+     #title p {
+            color: white;
+            font-size: 50px;
+            margin: 0;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-family: Georgia, 'Times New Roman', Times, serif;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+        }
+
+      .main1 {
+	 padding: 20px;
+        margin: 20px;
+        background-color: #fff;
+        border-radius: 8px;
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+        }
+
+      .main2 {
+	 padding: 20px;
+        margin: 20px;
+        background-color: #fff;
+        border-radius: 8px;
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+      }
+      
+      .main3 {
+	 padding: 20px;
+        margin: 20px;
+        background-color: #fff;
+        border-radius: 8px;
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+      }
+      
+      .main4 {
+	 padding: 20px;
+        margin: 20px;
+        background-color: #fff;
+        border-radius: 8px;
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+      }
+      
+      .main5 {
+	padding: 20px;
+        margin: 20px;
+        background-color: #fff;
+        border-radius: 8px;
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+        }
+
+      .food_title {
+	padding: 5px;
+        margin: 1px;
+	width: 100px;
+        background-color: #ffeff7;
+        border-radius: 8px;
+        }
+
+    .container {
+            width: 100%;
+            margin-top: 20px;
+            display: flex;
+            flex-wrap: wrap;
+            gap: 15px;
+            white-space: nowrap;
+        }
+
+        .box {
+            width: 500px;
+            padding: 10px;
+            box-sizing: border-box;
+	    padding: 20px;
+            margin: 20px;
+            background-color: #fff;
+            border-radius: 8px;
+        }
+
+       h1 {
+        font-size: 36px;
+        color: white;
+        letter-spacing: 5px;
+        margin: 0;
+      }
+
+       h2 {
+            text-align: center;
+            color: #ff006a;
+            margin: 30px 0;
+            font-size: 24px;
+            padding: 5px;
+       }
+
+         a {
+            text-decoration: none;
+            transition: 150ms
+        }
+
+       a:hover {
+        text-decoration: underline;
+        color: #ff4500;
+      }
+
+    
+  </style>
+</head>
+<body>
+  <div id = 'title'>
+  <p>福井まるわかり</p>
+  </div>
+ <div class = main1>
+  <H2>福井ってどこにあるの？</H2>
+  <IMG SRC="japanmap_fukui.jpg">
+  <IMG SRC="福井県(市町村).png" width="300" height="300">
+  <br>福井県は本州の中部に位置し、日本海に面しています。隣り合っている県からみると、京都府の北東、滋賀県の北、岐阜県の北西、石川県の南西にあります。
+  <br>
+  </div>
+  <br>
+
+ <div class = main2>
+  <H2>嶺北と嶺南</H2>
+  <IMG SRC = "2539_d006_l.jpg"><br>
+  敦賀市北東の木の芽峠を境として、北～北東部の「<font color = "ORANGE">嶺北地域</font>」、若狭湾に沿った南西部の「<font color = "GREEN">嶺南地域</font>」に分けられます。<br>嶺北と嶺南では同じ県にもかかわらず、文化が大きく異なります。例えば、方言でいうと嶺北は「抑揚がなく、平板なしゃべり方」、嶺南は「京都に近いしゃべり方」であり、ちょっと聞くだけでも違いがわかります。
+  <br>
+  </div>
+  <br>
+
+  <div class = main3>
+  <H2>気候</H2>
+  福井県は、冬に晴れの日が少なく、降水量が多い特徴を持つ日本海岸気候区に属します。特に嶺北地方の奥越では気温は低く、<font color = "orange">冬の雪が多い</font>特徴があり、平野部でも大雪となることがあります。<br>嶺南地方は、嶺北地方と比べると、雪雲の影響を受けにくく、<font color = "green">冬の降水量は少なめで、比較的あたたかい気候</font>となっています。
+  <br>
+  </div>
+  <br>
+
+  <div class = main4>
+  <H2>伝統工芸</H2>
+  <IMG SRC = "-2020-01-24-15.33.46-e1579847699851.png" whidth="800" height=""><br>
+  福井県内ではさまざまな伝統的工芸品がつくられています。<br>長いものでは1500年以上もの歴史があり、その土地の風土や歴史を反映した興味深く美しい工芸品ばかり！<br>特に<B>越前漆器、越前和紙、越前打刃物、越前焼、越前箪笥、若狭塗、若狭めのう細工</B>の７品目は経済産業大臣指定の伝統的工芸品として認められています。
+  <br>
+  </div>
+  <bn>
+
+  <div class = main5>
+  <H2>名物／ご当地グルメ</H2>
+    <IMG SRC = "ec8187d478afc5fac86fdfac839a2de5_t.jpeg" width="256" height="192"> 
+ <IMG SRC = "79_1_l.jpg" width = "287" height = "192">
+  <IMG SRC = "66499933aad2861d9f7cf6079e0c0ab8_t.jpeg" width="256" height="192">
+  <IMG SRC = "heraage2-750x500.jpg" width="288" height="192">
+  <IMG SRC = "img2.jpg" width="290" height="192">
+  <br>
+  <style>
+    span{
+     margin-right: 10px;
+    }
+  </style>
+  福井には<B>越前おろしそば</B>をはじめ、<br>
+<B><span>ソースカツ丼、</span><span>ボルガライス、</span><span>越前ガニ、</span><span>羽二重餅、</span><span>へしこ、</span><span>でっち羊羹、</span><span>麩の辛し和え、</span><span>すこ、</span><span>たくあんの煮たの、</span>はまなみそ</B>
+など、様々な名物があります。<br>
+その中でも越前そば、ソースカツ丼、九頭竜舞茸、油揚げは価格も比較的安く、人を選ばない味で家庭でも食べられるまさにソウルフードです。以下の店舗でこの４つの名物が販売されています。（クリックすると地図が表示されます）<br><br>
+
+<?php
+  $hostname = 'localhost';
+  $username = 'webdb';
+  $password = 'vTQC69P/';
+  $db = 'exp33_24_17';
+  $mysqli = new mysqli($hostname, $username, $password, $db);
+  $query = 'select * from food';
+  $result = $mysqli->query($query);
+
+echo "<div class='container'>";  // コンテナを開始
+  echo "<div class='box food'>";
+  echo "<div class='food_title'>";
+  echo '<B>越前そば</B>','<br>', '</div>';
+  for ($i = 0; $i < $result->num_rows; $i++) {
+      $result->data_seek($i);
+      $record = $result->fetch_assoc();
+      $menu = "そば";
+
+      if(strpos($record['menu'], $menu) !== false || strpos($record['menu2'], $menu) !== false){
+        echo "<a class='map-link' href='https://www.google.com/maps/search/?api=1&query=" . urlencode($record['address'] . ' ' . $record['name']) . "' target='_blank'>" . htmlspecialchars($record['name']) . "</a><br>";
+      }
+  }
+  echo '<br>','<br>';
+   echo "</div>";
+
+  echo "<div class='box food'>";
+  echo "<div class='food_title'>";
+  echo '<B>ソースカツ丼</B>','<br>', '</div>';
+  for ($i = 0; $i < $result->num_rows; $i++) {
+      $result->data_seek($i);
+      $record = $result->fetch_assoc();
+      $menu = "ソースカツ";
+
+      if(strpos($record['menu'], $menu) !== false || strpos($record['menu2'], $menu) !== false){
+         echo "<a class='map-link' href='https://www.google.com/maps/search/?api=1&query=" . urlencode($record['address'] . ' ' . $record['name']) . "' target='_blank'>" . htmlspecialchars($record['name']) . "</a><br>";
+      }
+  }
+  echo '<br>','<br>';
+ echo "</div>";
+
+  echo "<div class='box food'>";
+  echo "<div class='food_title'>";
+  echo '<B>九頭竜舞茸</B>','<br>', '</div>';
+  for ($i = 0; $i < $result->num_rows; $i++) {
+     $result->data_seek($i);
+      $record = $result->fetch_assoc();
+      $menu = "舞茸";
+      $menu2 = "まいたけ";
+
+      if(strpos($record['menu2'], $menu) !== false || strpos($record['menu2'], $menu2) !== false){
+         echo "<a class='map-link' href='https://www.google.com/maps/search/?api=1&query=" . urlencode($record['address'] . ' ' . $record['name']) . "' target='_blank'>" . htmlspecialchars($record['name']) . "</a><br>";
+      }
+  }
+  echo '<br>','<br>';
+  echo "</div>";
+
+  echo "<div class='box food'>";
+  echo "<div class='food_title'>";
+  echo '<B>油揚げ</B>','<br>','</div>';
+  for ($i = 0; $i < $result->num_rows; $i++) {
+      $result->data_seek($i);
+      $record = $result->fetch_assoc();
+      $menu = "厚揚げ";
+
+      if(strpos($record['menu'], $menu) !== false || strpos($record['menu2'], $menu) !== false){
+         echo "<a class='map-link' href='https://www.google.com/maps/search/?api=1&query=" . urlencode($record['address'] . ' ' . $record['name']) . "' target='_blank'>" . htmlspecialchars($record['name']) . "</a><br>";
+      }
+  }
+  echo '<br>','<br>';
+  echo "</div>";
+  echo "</div>";
+  $mysqli->close();
+?>
+
+  お店で食べるもよし、お土産で買うのもよし。伝統的な味わいから、現代的なアレンジまで、多彩な料理が楽しめる福井の食文化を堪能してください。<br>
+</div>
+  <br>
+  <a href="http://dsais03.dsai.u-fukui.ac.jp/~exp33-24-17/index.html"> 戻る</a>
+  <br>
+ 
+  
+</body>
+</html>
+
